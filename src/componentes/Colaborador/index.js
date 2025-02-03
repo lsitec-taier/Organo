cd0453cb456cfd64280f6bc8a74158c134345d08
lsitec-taier <marcio.taier@lsitec.org.br>
2025-02-03 17:35:50 -0300

    O que são componentes

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,17 +1,16 @@
-import './Colaborador.css'
+import './colaborador.css'
 
-const Colaborador = (props) => {
-    return (
-        <div className='colaborador'>
-            <div className='cabecalho' style={{backgroundColor: props.corDeFundo}}>
-                <img src={props.imagem} alt="Imagem do colaborador" />
-            </div>
-            <div className='rodape'>
-                <h4>{props.nome}</h4>
-                <h5>{props.cargo}</h5>
-            </div>
+const Colaborador = ({ colaborador, corDeFundo, aoDeletar }) => {
+    return (<div className="colaborador">
+        <div className='Deletar' onClick={aoDeletar}>deletar</div>
+        <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
+            <img src={colaborador.imagem} alt={colaborador.nome} />
         </div>
-    )
+        <div className="rodape">
+            <h4>{colaborador.nome}</h4>
+            <h5>{colaborador.cargo}</h5>
+        </div>
+    </div>)
 }
 
-export default Colaborador;
+export default Colaborador
