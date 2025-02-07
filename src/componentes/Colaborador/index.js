13ec705a3b4ed3bedac77517bf82c423f3750226
lsitec-taier <marcio.taier@lsitec.org.br>
2025-02-07 17:31:11 -0300

    Botao deletar e favoritar

--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -1,14 +1,34 @@
 import './colaborador.css'
+import { AiFillCloseCircle, AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 
-const Colaborador = ({ colaborador, corDeFundo, aoDeletar }) => {
+const Colaborador = ({ colaborador, corDeFundo, aoDeletar, aoFavoritar }) => {
+    function favoritar() {
+        aoFavoritar(colaborador.id)
+    }
+
+    const propsFavorito = {
+        size: 25,
+        onClick: favoritar
+    }
+
     return (<div className="colaborador">
-        <div className='Deletar' onClick={aoDeletar}>deletar</div>
+        <AiFillCloseCircle 
+            size={25} 
+            className='deletar' 
+            onClick={() => aoDeletar(colaborador.id)} 
+        />
         <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
             <img src={colaborador.imagem} alt={colaborador.nome} />
         </div>
         <div className="rodape">
             <h4>{colaborador.nome}</h4>
             <h5>{colaborador.cargo}</h5>
+            <div className='favoritar'>
+                {colaborador.favorito 
+                    ? <AiFillHeart {...propsFavorito} color='#ff0000' /> 
+                    : <AiOutlineHeart {...propsFavorito} />
+                }
+            </div>
         </div>
     </div>)
 }
